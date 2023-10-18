e02e9a13af8f97770491e43634deb471c66fea53
aarreumii <140951595+aareumii@users.noreply.github.com>
2023-10-18 13:32:59 +0900

    .env파일 위치 수정

--- coupang/src/api/index.ts
+++ coupang/src/api/index.ts
@@ -1,18 +1,19 @@
 import axios, { AxiosInstance } from 'axios';
 
 const axiosClient: AxiosInstance = axios.create({
-	baseURL: process.env.REACT_APP_BASE_URL, // REACT_APP_BASE_URL =   "http://43.201.30.126:8080/api"
+	baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 axiosClient.interceptors.response.use(
 	(response) => {
-		if (response.headers['authorization']) {
-			let accessToken = response.headers['authorization'];
+		if (response.headers['Authorization']) {
+			let accessToken = response.headers['Authorization'];
 
 			if (accessToken.startsWith('Bearer ')) {
 				accessToken = accessToken.slice(7);
 			}
 
+			// Consider using a more secure token storage method
 			localStorage.setItem('accessToken', accessToken);
 		}
 		return response;
@@ -26,14 +27,11 @@ axiosClient.interceptors.request.use(
 	(config) => {
 		const token = localStorage.getItem('accessToken');
 
+		// This may need to be set conditionally based on the specific request
 		config.headers['Content-Type'] = 'application/json; charset=utf-8';
 
 		if (token) {
-			if (config.url === '/user/logout') {
-				config.headers['token'] = token; // logout 요청의 경우 'token' 헤더에 토큰 값을 넣어줍니다.
-			} else {
-				config.headers['Authorization'] = `Bearer ${token}`; // 그 외 요청에 대해서는 "Bearer"를 포함하여 토큰을 넣어줍니다.
-			}
+			config.headers['Authorization'] = `Bearer ${token}`; // Use "Bearer" prefix for all requests
 		}
 
 		return config;
@@ -41,6 +39,6 @@ axiosClient.interceptors.request.use(
 	(error) => {
 		return Promise.reject(error);
 	}
-);
+); // <-- 이 부분이 누락되었었습니다.
 
 export default axiosClient;
