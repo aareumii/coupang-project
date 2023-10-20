312d4daf5b44cdb94ea40e0ea4ebe7a47bf81cba
aarreumii <140951595+aareumii@users.noreply.github.com>
2023-10-21 00:20:13 +0900

    chore:url 하드코딩(배포테스트)

--- coupang/src/api/index.ts
+++ coupang/src/api/index.ts
@@ -1,40 +1,40 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance } from 'axios';
 
 const axiosClient: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL, // REACT_APP_BASE_URL =   "http://43.201.30.126:8080/api"
+	baseURL: 'http://43.201.30.126:8080/api', // baseURL 프로퍼티를 사용하여 기본 URL 설정
 });
 
 axiosClient.interceptors.response.use(
-  (response) => {
-    if (response.headers["authorization"]) {
-      let accessToken = response.headers["authorization"];
+	(response) => {
+		if (response.headers['authorization']) {
+			let accessToken = response.headers['authorization'];
 
-      if (accessToken.startsWith("Bearer ")) {
-        accessToken = accessToken.slice(7);
-      }
+			if (accessToken.startsWith('Bearer ')) {
+				accessToken = accessToken.slice(7);
+			}
 
-      localStorage.setItem("accessToken", accessToken);
-    }
-    return response;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+			localStorage.setItem('accessToken', accessToken);
+		}
+		return response;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
 );
 
 axiosClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("accessToken");
+	(config) => {
+		const token = localStorage.getItem('accessToken');
 
-    if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
-    }
+		if (token) {
+			config.headers['Authorization'] = `Bearer ${token}`;
+		}
 
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
 );
 
 export default axiosClient;
